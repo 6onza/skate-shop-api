3ba0bc427a0290463bb027ed385c8f1e2200e035
6onza <gvineg@fi.uba.ar>
2023-06-18 01:10:24 -0300

    Fix: Updated read_only_fields to disallow modifications through endpoints

--- products/serializers.py
+++ products/serializers.py
@@ -8,7 +8,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
-        read_only_fields = '__all__'
+        read_only_fields = ['id', 'name', 'description', 'price', 'image_1','image_2', 'image_3', 'size', 'category', 'created_at', 'updated_at']
 
     def get_size(self, product):
         return product.size.values_list('name', flat=True)
