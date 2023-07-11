910bf52580cce06c5f56e89642a39d5adf601689
6onza <gvineg@fi.uba.ar>
2023-07-11 03:47:48 -0300

    Added http_method_names variable to allow GET and HEAD only

--- products/views.py
+++ products/views.py
@@ -3,17 +3,21 @@ from rest_framework.pagination import PageNumberPagination
 from .models import Product
 from .serializers import ProductSerializer
 from django.db.models import Q
-from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authentication import BaseAuthentication
+from django.conf import settings
 
 class CustomPagination(PageNumberPagination):
     page_size = 8
 
-class ProductsViewSet(viewsets.ModelViewSet):
+
+class ProductsViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ProductSerializer
     pagination_class = CustomPagination
     filter_backends = [filters.OrderingFilter]
     sort_by_fields = ['price', 'created_at']
     sort_by = '-created_at'  # Default ordering
+    http_method_names = ['get', 'head']
 
     def get_queryset(self):
         queryset = Product.objects.all()
@@ -23,7 +27,7 @@ class ProductsViewSet(viewsets.ModelViewSet):
 
         if categories == 'all':
             return queryset
-        
+
         if categories:
             category_list = categories.split(',')  # Obtener una lista de categorías separadas por coma
             queryset = queryset.filter(Q(category__in=category_list))
