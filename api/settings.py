b2b5d42bba50ff6b49b509ec234f34f7ece9e936
6onza <gvineg@fi.uba.ar>
2023-07-10 21:39:14 -0300

    Added JWTAuthentication to DEFAULT_AUTHENTICATION_CLASES

--- api/settings.py
+++ api/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 8
+    'PAGE_SIZE': 8,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 MIDDLEWARE = [
